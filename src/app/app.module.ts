d13eb774089330eaf08997d0a5e1a4f62b03c2ce
Kyle Koeller <kylek30844@gmail.com>
2020-12-19 15:13:30 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { GifsComponent } from './gifs/gifs.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    GifsComponent,
+    NotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
